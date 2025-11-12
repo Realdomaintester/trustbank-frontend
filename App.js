aee09c9fe2c94bdad26cfb8d1592a7df4cacedb4
Realdomaintester <fedexagentss3457@gmail.com>
2025-11-12 18:27:36 +0100

    Initialize React app with basic structure

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>TrustBank Frontend</h1>
+      <p>Your app is running successfully!</p>
+    </div>
+  );
+}
