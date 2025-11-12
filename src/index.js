aab561282a6101dfa3eee172eee00fa90197c862
Realdomaintester <fedexagentss3457@gmail.com>
2025-11-12 18:22:44 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Dashboard from "./Dashboard";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Dashboard />);
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
