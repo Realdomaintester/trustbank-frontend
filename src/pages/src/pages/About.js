9722edb77c830c5096ae356549171331569c84d3
Realdomaintester <fedexagentss3457@gmail.com>
2025-11-13 02:59:37 +0100

    Create About.js

---
+++ src/pages/src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h2>About TrustBank</h2>
+      <p>TrustBank is a modern banking platform built with React and deployed on Vercel.</p>
+    </div>
+  );
+}
